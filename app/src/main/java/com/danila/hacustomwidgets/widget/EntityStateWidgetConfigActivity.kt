2ba72999cc5b037324c797ae49d841282a4f9cb4
DannyNov <35140922+DannyNov@users.noreply.github.com>
2026-08-27 14:21:52 +0300

    fix: use Compose scoped weight modifiers

--- app/src/main/java/com/danila/hacustomwidgets/widget/EntityStateWidgetConfigActivity.kt
+++ app/src/main/java/com/danila/hacustomwidgets/widget/EntityStateWidgetConfigActivity.kt
@@ -10,11 +10,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -232,7 +232,7 @@ private fun DeviceList(groups: List<HaDeviceGroup>, query: String, onOpen: (HaDe
 }
 
 @Composable
-private fun EntitySelection(
+private fun ColumnScope.EntitySelection(
     group: HaDeviceGroup,
     query: String,
     selectedIds: Set<String>,
